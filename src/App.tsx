c15d584e2626f4d904713ba58475f0f071c6da8e
co6tter <co6tter@gmail.com>
2025-09-25 20:20:55 +0900

    feat: enhance note management with create, delete, and update functionalities in SideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
+import SideMenu from "./components/SideMenu";
+import { notesAtom } from "./store";
+import { useSetAtom } from "jotai";
+import { useEffect } from "react";
+import { Note } from "./domain/note";
 
 function App() {
-  const notes = useQuery(api.notes.get);
+  const setNotes = useSetAtom(notesAtom);
+  const initNotes = useQuery(api.notes.get);
+
+  useEffect(() => {
+    const notes = initNotes?.map(
+      (note) => new Note(note._id, note.title, note.content, note.lastEditTime)
+    );
+    setNotes(notes || []);
+  }, [initNotes, setNotes]);
 
   return (
-    <div className="App">
-      {notes?.map(({ _id, title }) => (
-        <div key={_id}>{title}</div>
-      ))}
+    <div>
+      <SideMenu />
     </div>
   );
 }
